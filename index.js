6fd15b8f2c57ae242a5fccf29dabf7bd4ae6cbc6
igor98santos <85197556+igor98santos@users.noreply.github.com>
2025-04-26 17:14:01 -0300

    methodos implementados

--- index.js
+++ index.js
@@ -1,25 +1,62 @@
-// import { MongoClient } from "mongodb";
+require("dotenv").config();
+const express = require("express");
+const { MongoClient, ObjectId } = require("mongodb");
+const cors = require("cors");
 
-const { MongoClient } = require("mongodb");
+const app = express();
+app.use(express.json());
+app.use(cors());
 
-// Replace the uri string with your connection string.
-const uri = "mongodb+srv://pedro:sYkDPBI8p0Vl0KLV@cluster0.bptzj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-
+const uri = process.env.MONGO_URI;
 const client = new MongoClient(uri);
 
-async function run() {
-  try {
-    const database = client.db('TCC');
-    const login = database.collection('login');
-
-    // Query for a movie that has the title 'Back to the Future'
-    const query = { name: 'Jane Doe' };
-    const movie = await login.findOne(query);
-
-    console.log(movie);
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
+async function connectDB() {
+    await client.connect();
+    console.log(" Conectado ao MongoDB");
 }
-run().catch(console.dir);
+connectDB();
+
+const database = client.db("TCC");
+const loginCollection = database.collection("login");
+
+//  [GET] - Buscar todos os usuários
+app.get("/usuarios", async (req, res) => {
+    const usuarios = await loginCollection.find().toArray();
+    res.json(usuarios);
+});
+
+// [POST] - Cadastrar novo usuário
+app.post("/usuarios", async (req, res) => {
+    const { email, senha } = req.body;
+    if (!email || !senha) {
+        return res.status(400).json({ erro: "Email e senha são obrigatórios!" });
+    }
+
+    const novoUsuario = await loginCollection.insertOne({ email, senha });
+    res.status(201).json({ mensagem: "Usuário cadastrado!", id: novoUsuario.insertedId });
+});
+
+//  [PUT] - Atualizar usuário por ID
+app.put("/usuarios/:id", async (req, res) => {
+    const id = req.params.id;
+    const { email, senha } = req.body;
+
+    await loginCollection.updateOne(
+        { _id: new ObjectId(id) },
+        { $set: { email, senha } }
+    );
+
+    res.json({ mensagem: "Usuário atualizado!" });
+});
+
+//  [DELETE] - Remover usuário por ID
+app.delete("/usuarios/:id", async (req, res) => {
+    const id = req.params.id;
+
+    await loginCollection.deleteOne({ _id: new ObjectId(id) });
+    res.json({ mensagem: "Usuário deletado!" });
+});
+
+// Iniciar servidor
+const PORT = 3000;
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
